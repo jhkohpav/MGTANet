2acdeba2c86f57c88530a77a7cf40ad5a1a8db81
HYjhkoh <jhkoh@spa.hanyang.ac.kr>
2022-12-02 15:59:05 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 cd det3d/ops/dcn 
 python setup.py build_ext --inplace
 
-cd .. && cd  iou3d_nms
+cd .. && cd iou3d_nms
 python setup.py build_ext --inplace
+
+cd .. && cd defromDETR
+python setup.py build install
+
+cd ../../../apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
